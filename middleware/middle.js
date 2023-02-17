cd8062efc1956adc7da2965f25268d477ac72ac2
Thite Shubham <115460300+ThiteShubham@users.noreply.github.com>
2023-02-17 17:01:36 +0530

    Update middle.js

--- middleware/middle.js
+++ middleware/middle.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const verify = (req,res,next)=>{
     let token = req.headers.authorization
-    if(token){jwt.verify(token,7nexx8374yx3y44r44rx4x84x4zmdiu8z39,(err,deco)=>{
+    if(token){jwt.verify(token,"7nexx8374yx3y44r44rx4x84x4zmdiu8z39",(err,deco)=>{
         if(deco){
             req.body.user = deco.userid
             next()
